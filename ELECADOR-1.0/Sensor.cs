d3af24e3e70bddadde58a167215c1dff090f3fb3
Angel Araujo <angel1.araujo2004@gmail.com>
2025-07-07 09:03:13 -0600

    cambie el sensor Angel

--- ELECADOR-1.0/Sensor.cs
+++ ELECADOR-1.0/Sensor.cs
@@ -2,6 +2,25 @@
 {
     public partial class Sensor
     {
+        public bool detectarPeso()
+        {
+            Console.WriteLine("Ingrese el peso actual del elevador en Kg: ");
+            string pesoActual = Console.ReadLine();
+            float pesoActualNum = Convert.ToSingle(pesoActual);
 
+            if (pesoActualNum >= 700)
+            {
+                Console.WriteLine("Peso no permitido, excede limite de peso");
+                Console.WriteLine("¡Favor de bajar una persona o quitar peso del elevador!");
+                return false;
+            }
+
+            else
+            {
+                Console.WriteLine("Peso no permitido, excede limite de peso");
+                Console.WriteLine("¡Favor de bajar una persona o quitar peso del elevador!");
+                return true;
+            }
+        }
     }
 }
